b53088002e29f1a495fe880b3e715cbc18307f79
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 13:02:00 -0600

    ejercicio 6 terminado

--- ejercicio 6/Program.cs
+++ ejercicio 6/Program.cs
@@ -1,1 +1,11 @@
 ﻿int n = 3;
+int contador = 1;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(contador + "  ");
+        contador++;
+    }
+    Console.WriteLine();
+}
