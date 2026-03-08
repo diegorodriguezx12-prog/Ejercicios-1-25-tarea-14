c6c4ae23ea796936e9596ce3023ba4782f8c3960
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 12:49:46 -0600

    ejercicio 5 terminado

--- ejercicio 5/Program.cs
+++ ejercicio 5/Program.cs
@@ -1,1 +1,12 @@
 ﻿int limiteTablas = 5;
+int limiteMultiplicador = 10;
+for (int i = 1; i <= limiteTablas; i++)
+{
+    Console.WriteLine($"--- Tabla del {i} ---");
+    for (int j = 1; j <= limiteMultiplicador; j++)
+    {
+        int resultado = i * j;
+        Console.WriteLine($"{i} x {j} = {resultado}");
+    }
+    Console.WriteLine();
+}
