513a3521de5af7b85912cdfc064c00e453a0f596
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 13:18:16 -0600

    ejercicio 8 terminado

--- ejercicio 8/Program.cs
+++ ejercicio 8/Program.cs
@@ -1,1 +1,9 @@
 ﻿int total = 4;
+for (int fila = 1; fila <= total; fila++)
+{
+    for (int columna = 1; columna <= total; columna++)
+    {
+        Console.Write($"({fila},{columna})  ");
+    }
+    Console.WriteLine();
+}
