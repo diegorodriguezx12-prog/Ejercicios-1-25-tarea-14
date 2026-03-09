8dfc7e6d33a425ea7e3baf2a0ce9df05474144c5
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 20:33:55 -0600

    ejercicio 25 terminado

--- ejercicio 25/Program.cs
+++ ejercicio 25/Program.cs
@@ -1,1 +1,26 @@
 ﻿int opcion = 0;
+while (opcion != 3)
+{
+    Console.WriteLine("--- MENÚ CON WHILE ---");
+    Console.WriteLine("1. Saludar");
+    Console.WriteLine("2. Ver fecha actual");
+    Console.WriteLine("3. Salir");
+    Console.Write("Seleccione una opción: ");
+    opcion = int.Parse(Console.ReadLine());
+    if (opcion == 1)
+    {
+        Console.WriteLine("¡Hola! Estás usando un ciclo while.");
+    }
+    else if (opcion == 2)
+    {
+        Console.WriteLine("Hoy es: ");
+    }
+    else if (opcion == 3)
+    {
+        Console.WriteLine("Saliendo del programa...");
+    }
+    else
+    {
+        Console.WriteLine("Opción no válida.");
+    }
+}
