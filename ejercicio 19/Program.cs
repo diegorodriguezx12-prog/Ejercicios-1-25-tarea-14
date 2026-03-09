5608f2609bba6814ae1d1cbc5dad432dc112394e
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 19:50:26 -0600

    ejercicio 19 terminado

--- ejercicio 19/Program.cs
+++ ejercicio 19/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.Write("¿Qué tabla de multiplicar deseas ver?: ");
+int tabla = int.Parse(Console.ReadLine());
+int i = 1;
+Console.WriteLine($"--- Tabla del {tabla} ---");
+while (i <= 10)
+{
+    int resultado = tabla * i;
+    Console.WriteLine($"{tabla} x {i} = {resultado}");
+    i++;
+}
