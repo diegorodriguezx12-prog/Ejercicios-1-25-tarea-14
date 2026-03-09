0309e55c95ed9da3a8b70c2b03d2299a9bf10e90
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 19:34:35 -0600

    Ejercicio 17 terminado

--- ejercicio 17/Program.cs
+++ ejercicio 17/Program.cs
@@ -1,1 +1,13 @@
 ﻿int numero;
+int contadorPositivos = 0;
+do
+{
+    Console.Write("Ingrese un número (0 para salir): ");
+    numero = int.Parse(Console.ReadLine());
+    if (numero > 0)
+    {
+        contadorPositivos++;
+    }
+
+} while (numero != 0);
+Console.WriteLine($"\nCantidad de números positivos ingresados: {contadorPositivos}");
