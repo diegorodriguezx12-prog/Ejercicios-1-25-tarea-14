3f510e2e46caa1323c6bbf384b5213ae03c70fd9
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 20:05:49 -0600

    ejercicio 21 terminado

--- ejercicio 21/Program.cs
+++ ejercicio 21/Program.cs
@@ -1,1 +1,11 @@
 ﻿int suma = 0;
+int numero;
+Console.WriteLine("Ingresa números para sumar. El programa se detendrá al superar 100.");
+while (suma <= 100)
+{
+    Console.Write($"Suma actual ({suma}). Ingrese un número: ");
+    numero = int.Parse(Console.ReadLine());
+    suma += numero;
+}
+Console.WriteLine("¡Límite superado!");
+Console.WriteLine($"La suma final es: {suma}");
