507880a424439550c958d5f95d15c2d19db3efd3
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 20:11:24 -0600

    ejercicio 22 terminado

--- ejercicio 22/Program.cs
+++ ejercicio 22/Program.cs
@@ -1,1 +1,12 @@
 ﻿string claveCorrecta = "admin123";
+string intento = "";
+while (intento != claveCorrecta)
+{
+    Console.Write("Ingrese la contraseña: ");
+    intento = Console.ReadLine();
+    if (intento != claveCorrecta)
+    {
+        Console.WriteLine("Contraseña incorrecta. Intente de nuevo.\n");
+    }
+}
+Console.WriteLine("¡Acceso concedido!");
