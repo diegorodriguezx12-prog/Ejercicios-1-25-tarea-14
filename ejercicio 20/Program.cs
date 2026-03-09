d796eeb656b037add8ee7a47ab41196bb4010d18
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 20:00:23 -0600

    ejercicio 20 terminado

--- ejercicio 20/Program.cs
+++ ejercicio 20/Program.cs
@@ -1,1 +1,16 @@
 ﻿Console.Write("Ingrese un número entero: ");
+long numero = Math.Abs(long.Parse(Console.ReadLine()));
+int contadorDigitos = 0;
+if (numero == 0)
+{
+    contadorDigitos = 1;
+}
+else
+{
+    while (numero > 0)
+    {
+        numero = numero / 10;
+        contadorDigitos++;
+    }
+}
+Console.WriteLine($"El número tiene {contadorDigitos} dígitos.");
