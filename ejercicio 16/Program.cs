baada5bb49bdbbf564eae24973070a7923050f1c
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 19:29:42 -0600

    ejercicio 16 terminado

--- ejercicio 16/Program.cs
+++ ejercicio 16/Program.cs
@@ -1,1 +1,12 @@
 ﻿int numero = -1;
+while (numero != 0)
+{
+    Console.Write("Ingrese un número (0 para salir): ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero != 0)
+    {
+        Console.WriteLine($"Ingresaste el: {numero}");
+    }
+}
+Console.WriteLine("Programa finalizado.");
