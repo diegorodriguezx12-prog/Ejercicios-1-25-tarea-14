511f28696ad5972f3de2d55a0804fde19468057f
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-08 19:43:32 -0600

    ejercicio 18 terminado

--- ejercicio 18/Program.cs
+++ ejercicio 18/Program.cs
@@ -1,1 +1,17 @@
 ﻿Console.Write("Ingrese un número para calcular su factorial: ");
+int n = int.Parse(Console.ReadLine());
+long factorial = 1;
+int i = n;
+if (n < 0)
+{
+    Console.WriteLine("El factorial no está definido para números negativos.");
+}
+else
+{
+    while (i > 1)
+    {
+        factorial *= i;
+        i--;
+    }
+    Console.WriteLine($"El factorial de {n} es: {factorial}");
+}
